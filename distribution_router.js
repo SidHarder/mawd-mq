0e1743eb9bbfafc07eeec5dc8c188f2ee7f01d02
sidharder <sideharder@gmail.com>
2022-06-02 07:07:10 -1000

    working on publishing report

--- distribution_router.js
+++ distribution_router.js
@@ -3,6 +3,7 @@ import moment from 'moment';
 import mawdApi from './mawd_api.js';
 import distributionHoldupQueue from './distribution_holdup_queue.js';
 import reportPublishingQueue from './report_publishing_queue.js';
+import webDistributionQueue from './web_distribution_queue.js';
 
 const distributionRouter = {};
 
@@ -21,21 +22,14 @@ distributionHoldupQueue.worker.on('completed', async (job) => {
 });
 
 reportPublishingQueue.worker.on('completed', async (job) => {
-  console.log(`Report Publishing Complete for: ${job.data.accessionOrder.masterAccessionNo}`)
-  await handleDistribution(job.data.reportNo, job.data.accessionOrder);
+  console.log(`Report Publishing Completed for: ${job.data.reportNo}`)
+  webDistributionQueue.queue.add('HandleWebDistribution', jobData);  
+  //await handleDistribution(job.data.reportNo, job.data.accessionOrder);
 });
 
-async function handleDistribution(reportNo, accessionOrder) {
-  accessionOrder.testOrders.find(t => t.reportNo == reportNo).testOrderReportDistribution.forEach(d => {
-    if(d.distributionType == 'Web') {
-      d.distributed = true;
-      d.timeOfLastDistribution = moment().format('YYYYMMDDHHmmss')
-    }    
-  });;
-
-  accessionOrder.distributed = true;
-  var updatedAo = await mawdApi.updateAccessionOrder(accessionOrder);
-}
+webDistributionQueue.worker.on('completed', async (job) => {
+  console.log(`Web distribution completed for: ${job.data.reportNo}`)  
+});
 
 distributionRouter.submitJob = submitJob;
 
