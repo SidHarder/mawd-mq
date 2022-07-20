2926198c9b62dca8ae192f743a4bd03c3747d9cc
sidharder <sideharder@gmail.com>
2022-07-19 16:57:13 -1000

    turning on the fax

--- fax_distribution_queue.js
+++ fax_distribution_queue.js
@@ -1,5 +1,6 @@
 import moment from 'moment';
 import cron from 'node-cron';
+import fetch from 'node-fetch';
 
 import { Queue } from 'bullmq';
 import { Worker } from 'bullmq';
@@ -28,28 +29,24 @@ async function SendFax(faxJob) {
   var url = `${process.env.HTTP_SINGLE_FAX_URL}${faxJob.reportNo}`;
   console.log(`Faxing: ${url}`);  
 
-  /*
   try {
     const response = await fetch(url);
     const body = await response.text();
     console.log(body);
   } catch (e) {
     console.log(e);
-  }
-  */
+  }  
 }
 
-async function addFaxJob(data) {
-  console.log('Not adding faxes at this time.');
-  /*
+async function addFaxJob(data) {    
   var testOrder = data.accessionOrder.testOrders.find(t => t.reportNo == data.reportNo);
   var faxDistributions = testOrder.testOrderReportDistribution.filter(t => t.distributionType == 'Fax' && t.distributed == false);
+  
   for(var i=0; i<faxDistributions.length; i++) {
     var faxJob = { reportNo: faxDistributions[i].reportNo, faxNumber: faxDistributions[i].faxNumber, clientName: faxDistributions[i].clientName };
     await queue.add('FaxDistribution', faxJob);
     console.log(`Adding fax distribution for: ${faxJob.reportNo} - ${faxJob.clientName}`);    
   } 
-  */ 
 }
 
 const faxDistributionQueue = {};
