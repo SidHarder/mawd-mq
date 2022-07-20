9f1c639d79a5a89e071273a2addc04d75a6736f3
sidharder <sideharder@gmail.com>
2022-07-20 13:05:16 -1000

    removing delay

--- distribution_holdup_queue.js
+++ distribution_holdup_queue.js
@@ -3,13 +3,15 @@ import { Worker } from 'bullmq';
 import IORedis from 'ioredis';
 
 const connection = new IORedis({ port: 6379, host: "127.0.0.1", db: process.env.REDIS_DB, maxRetriesPerRequest: null });
+const schedulerConn = new IORedis({ port: 6379, host: "127.0.0.1", db: process.env.REDIS_DB, maxRetriesPerRequest: null });
+
 const queue = new Queue('Distribution_Holdup', {
   connection,
   defaultJobOptions: { removeOnComplete: true }
 });
 
 const worker = new Worker('Distribution_Holdup', handleJob, { connection });
-const queueScheduler = new QueueScheduler('Distribution_Holdup', { connection });
+const queueScheduler = new QueueScheduler('Distribution_Holdup', { schedulerConn });
 
 async function handleJob(job) {      
   console.log(job);
